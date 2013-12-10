6dda66ad4d1467f13038d2184137475771818c7a
materechm <materechm@gmail.com>
2013-12-10 15:36:05 -0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,27 @@
+
+import csv
+
+path = '/Users/mtchavez/Documents/ALS/Diseasome/GWAS.txt'
+read_file = open(path)
+reader = csv.reader(read_file, delimiter = '\t')
+fieldnames = reader.next()
+
+rows = list(reader)
+read_file.close()
+
+write_file = open('datatest.csv', 'wb')
+writer = csv.writer(write_file, delimiter = '\t')
+writer.writerow(('disease', 'genes'))
+
+disease_gene = dict()
+for row in rows:
+	disease = row[7]
+	reported_genes = row[13]
+	if reported_genes == 'NR':
+		reported_genes = row[14]
+	if reported_genes == 'Intergenic':
+		reported_genes = row[14]
+	disease_gene[disease] = reported_genes
+
+print disease_gene
+print disease_gene['Amyotrophic lateral sclerosis']
